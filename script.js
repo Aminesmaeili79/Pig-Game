d937a1b9d22c834b2c66ec9e136f235fee9c1f3b
Amin Esmaeili <aminesmaeili79@yahoo.com>
2025-04-10 19:36:51 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,59 @@
+'use strict';
+
+var activePlayer = 0;
+var playersScores = [0, 0];
+var currentScore = 0;
+
+const checkWinner = () => {
+    if (playersScores[activePlayer] >= 100) {
+        document.querySelector(`.player--${activePlayer}`).classList.toggle('player--winner');
+        document.querySelector(".btn--roll").disabled = true;
+        document.querySelector('.btn--hold').disabled = true;
+    }
+}
+
+const switchPlayer = () => {
+    document.querySelector(`.player--${activePlayer}`).classList.toggle("player--active");
+    activePlayer = activePlayer ? 0 : 1;
+    document.querySelector(`.player--${activePlayer}`).classList.toggle("player--active");
+}
+
+const handleRollDice = () => {
+    var randomNumber = Math.ceil(Math.random() * 6);
+    document.querySelector('.dice').classList.remove("hidden");
+    document.querySelector('.dice').src = `./assets/dice-${randomNumber}.png`
+    if (randomNumber === 1) {
+        currentScore = 0;
+        document.querySelector(`#current--${activePlayer}`).innerText = currentScore;
+        switchPlayer();
+    } else {
+        currentScore += randomNumber;
+        document.querySelector(`#current--${activePlayer}`).innerText = currentScore;
+    }
+}
+
+const handleHold = () => {
+    playersScores[activePlayer] += currentScore;
+    currentScore = 0;
+    document.querySelector(`#current--${activePlayer}`).innerText = 0;
+    document.getElementById(`score--${activePlayer}`).innerText = playersScores[activePlayer];
+    checkWinner();
+    switchPlayer();
+}
+
+const initGame = () => {
+    currentScore = 0;
+    playersScores = [0, 0];
+    activePlayer = 0;
+    document.getElementById('score--0').innerText = 0;
+    document.getElementById('score--1').innerText = 0;
+    document.querySelector(".dice").classList.add("hidden");
+    document.querySelector('.player--0').classList.remove('player--winner');
+    document.querySelector('.player--1').classList.remove('player--winner');
+
+    document.querySelector(".btn--roll").addEventListener('click', handleRollDice);
+    document.querySelector('.btn--hold').addEventListener('click', handleHold)
+    document.querySelector('.btn--new').addEventListener('click', initGame)
+}
+
+initGame();
